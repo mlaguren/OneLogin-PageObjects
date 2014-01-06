84338968ebc61a7df8c1bc3a4ba13eb28846a3fb
Melvin Laguren <melvin@onelogin.com>
2014-01-06 12:32:19 -0800

    Made changes for define_methods to be documented

--- common/NavigationBar.rb
+++ common/NavigationBar.rb
@@ -5,29 +5,31 @@ module NavigationBar
     @main_login = {"selector" => :id, "value" => "main_logo"}
   end
 
-  ["Users", "Apps", "Activity", "Settings"].each do |menu|
-    define_method("select_#{menu}") do
-      within(@container['selector'], @container['value']) do
-        click_link("#{menu}")
-      end
-    end
-  end
-
-  ["Users", "Apps", "Activity", "Settings"].each do |menu|
-    define_method("select_from_#{menu}_menu") do |submenu|
-      within(@container['selector'], @container['value']) do
-        begin
-          find(:xpath, "//*[@class='#{menu.downcase} dropdown']").hover
-          click_link("#{submenu}")
-          sleep 5
-        rescue
-          find(:xpath, "//*[@class='dropdown #{menu.downcase}']").hover
-          click_link("#{submenu}")
-          sleep 5
+  def select_menu
+    ["Users", "Apps", "Activity", "Settings"].each do |menu|
+      define_method("select_#{menu}") do
+        within(@container['selector'], @container['value']) do
+          click_link("#{menu}")
         end
       end
     end
   end
-          
+
+  def select_submenu
+    ["Users", "Apps", "Activity", "Settings"].each do |menu|
+      define_method("select_from_#{menu}_menu") do |submenu|
+        within(@container['selector'], @container['value']) do
+          begin
+            find(:xpath, "//*[@class='#{menu.downcase} dropdown']").hover
+            click_link("#{submenu}")
+          rescue
+            find(:xpath, "//*[@class='dropdown #{menu.downcase}']").hover
+            click_link("#{submenu}")
+          end
+        end
+      end
+    end
+  end
+        
 end
 
