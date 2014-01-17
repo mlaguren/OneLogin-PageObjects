773e33481d95954bb79dc4681c6b53c13dc7d3e2
Melvin Laguren <melvin@onelogin.com>
2014-01-17 13:49:20 -0800

    Methods to select from More Actions Menu

--- pages/UsersPage.rb
+++ pages/UsersPage.rb
@@ -11,5 +11,16 @@ class UsersPage
     click_link("New User")
   end
 
+# select_<submenu>_from_more_actions_menu  where <submenu> is either Import Users, Bulk Operations, Custom User Fields, Approval All Users
+# @method select_<submenu>_from_more_actions_menu
+
+  ["import_users", "bulk_operations", "custom_user_fields", "approve_all_users"].each do |sub_menu|
+    define_method("select_#{sub_menu}_from_more_actions_menu") do 
+      within(:xpath, ".//*[@class='btn-toolbar pull-right nav']") do
+        find(:id, 'more-actions').hover
+        click_link("#{sub_menu.gsub!(/_+/, " ").split.each{|i| i.capitalize!}.join(' ')}")
+      end
+    end
+  end
 end
 
