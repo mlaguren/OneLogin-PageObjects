0792c4e79ab1465556d1f3bb8c311a2c39c64537
Melvin Laguren <melvin@onelogin.com>
2014-03-03 16:18:54 -0800

    Fixing API page for getting new key

--- pages/APIPage.rb
+++ pages/APIPage.rb
@@ -18,7 +18,7 @@ class APIPage
   def generate_new_api_key(original_key)
     click_link("Generate new API key")
     key = find(:id, "account_api_key").value
-    until key == original_key
+    until key != original_key
       key = find(:id, "account_api_key").value
     end
     $log.debug("New Key: #{key}")
