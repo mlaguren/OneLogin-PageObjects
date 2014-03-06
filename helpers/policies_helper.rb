dbc1ee73f439e2f8c11a0dccecbb33d2479c1e74
Melvin Laguren <melvin@onelogin.com>
2014-03-05 17:17:28 -0800

    Removed prints in favor of log

--- helpers/policies_helper.rb
+++ helpers/policies_helper.rb
@@ -5,27 +5,36 @@ require 'active_support/core_ext/numeric/time.rb'
 #   1.minute executes in 1 second
 #    wait = timeout.gsub(/\s+/, ".")
 #    sleep(wait.to_i)
-    p Time.now
+    $log.debug("Starting the wait now:  #{Time.now}")
     if timeout == "1 minute"
+      $log.debug("Waiting for 1 minute and 1 second")
       sleep(61)
     elsif timeout == "15 minutes"
+      $log.debug("Waiting for 15 minutes and 1 second")
       sleep(901)
     elsif timeout == "30 minutes"
+      $log.debug("Waiting for 30 minutes and 1 second")
       sleep(1801)
     elsif timeout == "1 hour"
+      $log.debug("Waiting for 1 hour and 1 second")
       sleep(3601)
     elsif timeout == "2 hours"
+      $log.debug("Waiting for 2 hours and 1 second")
       sleep(7201)
     elsif timeout == "4 hours"
+      $log.debug("Waiting for 4 hours  and 1 second")
       sleep(14401)
     elsif timeout == "8 hours"
+      $log.debug("Waiting for 8 hours and 1 second")
       sleep(28801) 
     elsif timeout == "1 day"
+      $log.debug("Waiting for 1 day and 1 second")
       sleep(87301)
     else
-      sleep(10)
+      $log.debug("Invalid choice, failing the scenario")
+      exit 1
     end
-    p Time.now
+    $log.debug("Time is up: #{Time.now}")
   end
 
 
