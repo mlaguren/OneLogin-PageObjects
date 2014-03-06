bf2dd2ed77a255c7d150a18239fbc0611bdf5485
Melvin Laguren <melvin@onelogin.com>
2014-03-06 12:09:12 -0800

    Adding message for when an app is not configured for the account

--- pages/ClientAppsPage.rb
+++ pages/ClientAppsPage.rb
@@ -25,6 +25,10 @@ class ClientAppsPage
   def select_any_app
       sleep 5
       icon = all(@app['selector'], @app['value'])
+      if icon.length == 0
+        p "No apps configured"
+        $log.debug("Unable to select an app because an app is not configured for this account")
+      end
       icon.first.click
   end
     
