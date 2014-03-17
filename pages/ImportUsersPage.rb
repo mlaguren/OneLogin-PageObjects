f9f7d913dd1c86762b9095544f3d4018159bb909
Melvin Laguren <melvin@onelogin.com>
2014-03-17 12:17:24 -0700

    Added Menus

--- pages/ImportUsersPage.rb
+++ pages/ImportUsersPage.rb
@@ -1,12 +1,14 @@
 class ImportUsersPage
   include Capybara::DSL
+  include UserMenu
+  include NavigationBar
 
   def initialize
 
   end
 
   def upload_csv
-    p Dir.getwd + '/#{$csvFile}'
+    $log.debug("Dir.getwd + '/#{$csvFile}'")
     sleep 2
     page.driver.browser.all(:xpath, ".//*[@class='file-input-wrapper btn']/input")[0].send_keys(Dir.getwd + "/#{$csvFile}")
     click_button('Upload')
