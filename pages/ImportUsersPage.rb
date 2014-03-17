0cb67acba4f00190d979c056b3c32d8c336498a7
Melvin Laguren <melvin@onelogin.com>
2014-03-17 11:47:33 -0700

    Updated Import Users Page to use new csv tag

--- pages/ImportUsersPage.rb
+++ pages/ImportUsersPage.rb
@@ -6,9 +6,9 @@ class ImportUsersPage
   end
 
   def upload_csv
-    p Dir.getwd + '/test.csv'
+    p Dir.getwd + '/#{$csvFile}'
     sleep 2
-    page.driver.browser.all(:xpath, ".//*[@class='file-input-wrapper btn']/input")[0].send_keys(Dir.getwd + '/test.csv')
+    page.driver.browser.all(:xpath, ".//*[@class='file-input-wrapper btn']/input")[0].send_keys(Dir.getwd + "/#{$csvFile}")
     click_button('Upload')
   end
 
