d4c9f9ca15712de7807e566aeb88bd51873c69a4
Melvin Laguren <melvin@onelogin.com>
2014-04-01 15:33:05 -0700

    Adding new page object

---
+++ pages/UserPage.rb
@@ -0,0 +1,20 @@
+class UserPage
+  include UserMenu
+  include NavigationBar
+  include Capybara::DSL
+
+  def initialize
+    super
+  end 
+
+
+  def add_privilege(privilege)
+    find(:id, 'user-privileges').click
+    within(:id, 'popup_content'){
+      find(:xpath, './/*[@class="select2-choice"]').click
+    }
+    text = find(:xpath, './/*[@id='s2id_operation_id']//*[@class='select2-chosen']').text
+    sleep 5
+  end
+
+end
